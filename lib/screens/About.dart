1fefb13b8a5f1daa1b76c741b9ffe199638e9c0e
Ratnesh KArbhari <ratneshkarbhari74@gmail.com>
2021-03-25 17:09:31 +0530

    contact page done

--- lib/screens/About.dart
+++ lib/screens/About.dart
@@ -1,6 +1,11 @@
+import 'package:astro_app/templates/AppBarTemplate.dart';
+import 'package:astro_app/templates/DrawerTemplate.dart';
+import 'package:astro_app/templates/DrawerTemplateGeneral.dart';
 import 'package:flutter/material.dart';
-// import '../templates/AppBarTemplate.dart';
-// import '../templates/DrawerTemplate.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import './Service.dart';
+
 
 class About extends StatefulWidget {
   @override
@@ -8,36 +13,63 @@ class About extends StatefulWidget {
 }
 
 class _AboutState extends State<About> {
-  _AboutState();
 
+  var firstName;
+  var lastName;
+  var email;
+
+  Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+
+  Future<void> _setUserState() async {
+    final SharedPreferences prefs = await _prefs;
+    setState(() {
+      firstName = prefs.getString("first_name");
+      lastName = prefs.getString("last_name");
+      email = prefs.getString("email");
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _setUserState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // drawer: DrawerTemplate(),
-      // appBar: AppBarTemplate(this.name),
-      body: AboutPage(),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBarTemplate("About"),
+        drawer: (firstName==null)?DrawerTemplateGeneral():DrawerTemplate(firstName, lastName, email),
+        body: AboutPage(),
+      ),      
     );
   }
 }
 
 class AboutPage extends StatefulWidget {
-
-
   @override
   _AboutPageState createState() => _AboutPageState();
 }
 
 class _AboutPageState extends State<AboutPage> {
 
+    
+  
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Column(
-        children: [
-          
-        ],
-      ),
+      child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+          children: [
+            SizedBox(height: 10.0),
+            Text("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tinci",style: TextStyle(fontSize: 15.0)),
+            SizedBox(height: 10.0),
+            Text("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tinci",style: TextStyle(fontSize: 15.0)),
+          ],
+        ),
+      ),      
     );
   }
 }
--- lib/screens/Contact.dart
+++ lib/screens/Contact.dart
@@ -1,6 +1,9 @@
+import 'package:astro_app/templates/AppBarTemplate.dart';
+import 'package:astro_app/templates/DrawerTemplate.dart';
+import 'package:astro_app/templates/DrawerTemplateGeneral.dart';
 import 'package:flutter/material.dart';
-// import '../templates/AppBarTemplate.dart';
-// import '../templates/DrawerTemplate.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 
 class Contact extends StatefulWidget {
   @override
@@ -8,36 +11,69 @@ class Contact extends StatefulWidget {
 }
 
 class _ContactState extends State<Contact> {
-  _ContactState();
 
+  var firstName;
+  var lastName;
+  var email;
+
+  Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+
+  Future<void> _setUserState() async {
+    final SharedPreferences prefs = await _prefs;
+    setState(() {
+      firstName = prefs.getString("first_name");
+      lastName = prefs.getString("last_name");
+      email = prefs.getString("email");
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _setUserState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // drawer: DrawerTemplate(),
-      // appBar: AppBarTemplate(this.name),
-      body: ContactPage(),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBarTemplate("Contact"),
+        drawer: (firstName==null)?DrawerTemplateGeneral():DrawerTemplate(firstName, lastName, email),
+        body: ContactPage(),
+      ),      
     );
   }
 }
 
 class ContactPage extends StatefulWidget {
-
-
   @override
   _ContactPageState createState() => _ContactPageState();
 }
 
 class _ContactPageState extends State<ContactPage> {
 
+    
+  
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Column(
-        children: [
-          
-        ],
-      ),
+      child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+          children: [
+            SizedBox(height: 10.0),
+            Text("Mobile Number: ",style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold)),
+            Text("+91 9137976398",style: TextStyle(fontSize: 15.0)),
+            SizedBox(height: 10.0),
+            Text("Email: ",style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold)),
+            Text("ratneshkarbhari74@gmail.com",style: TextStyle(fontSize: 15.0)),
+            SizedBox(height: 10.0),
+            Text("Address: ",style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold)),
+            Text("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.",style: TextStyle(fontSize: 15.0)),
+            Text("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.",style: TextStyle(fontSize: 15.0)),
+          ],
+        ),
+      ),      
     );
   }
 }
